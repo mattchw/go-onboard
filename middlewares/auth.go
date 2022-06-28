d5b330188436fa85ef143be101b1be117651bdf2
Matthew Wong <matthew.chohin@gmail.com>
2022-06-28 17:03:30 +0800

    feat: add basic auth in create user route

---
+++ middlewares/auth.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
+)
+
+// AuthReq middleware
+func AuthReq() func(*fiber.Ctx) error {
+	return basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"admin": "12345678",
+		},
+	})
+}
--- routes/user_route.go
+++ routes/user_route.go
@@ -3,11 +3,12 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/mattchw/go-onboard/controllers"
+	middleware "github.com/mattchw/go-onboard/middlewares"
 )
 
 func UserRoute(app *fiber.App) {
 	app.Get("/users", controllers.GetUsers)
-	app.Post("/users", controllers.CreateUser)
+	app.Post("/users", middleware.AuthReq(), controllers.CreateUser)
 	app.Get("/users/:userId", controllers.GetUser)
 	app.Patch("/users/:userId", controllers.UpdateUser)
 	app.Delete("/users/:userId", controllers.DeleteUser)
