f3a4d6b563329812966702b13472682d534eb261
Matthew Wong <matthew.chohin@gmail.com>
2022-06-28 16:42:13 +0800

    feat: update main func to use user route

--- main.go
+++ main.go
@@ -1,69 +1,42 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/timeout"
 	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"github.com/mattchw/go-onboard/routes"
 )
-
-type User struct {
-	ID   string `bson:"_id"`
-	Name string `bson:"name"`
-}
-
-// Connection URI
-const uri = "mongodb://localhost:27017"
-
-var collection *mongo.Collection
-var ctx = context.TODO()
 
 // Main function
 func main() {
 	fmt.Println("!... Hello World ...!")
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-	// Ping the primary
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-		panic(err)
-	}
 
-	collection = client.Database("go-onboarding").Collection("users")
-
-	logrus.SetLevel(logrus.TraceLevel)
-	logrus.SetFormatter(&logrus.JSONFormatter{})
-	logrus.Info("info msg")
-
-	// show log with fields and messages
-	logrus.WithFields(logrus.Fields{
-		"animal": "walrus",
-		"size":   10,
-	}).Info("A group of walrus emerges from the ocean")
-
+	// load env file
 	envErr := godotenv.Load()
 	if envErr != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		AppName: "Go onboard v1.0.0",
+	})
 
+	// recover from any panics
+	app.Use(recover.New())
+	// default timeout
+	app.Use(timeout.New(func(c *fiber.Ctx) (err error) { return c.Next() }, 1*time.Second))
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		time.Sleep(5 * time.Second)
 		return c.SendString("OK!!!")
 	})
+	// routes
+	routes.UserRoute(app)
 
 	port := os.Getenv("PORT")
 	app.Listen(":" + port)
