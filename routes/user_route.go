ede93b3f6799f1543ee1d026c79c6cf199f87ab6
Matthew Wong <matthew.chohin@gmail.com>
2022-06-29 12:01:45 +0800

    fix: rename middleware to middlewares

--- routes/user_route.go
+++ routes/user_route.go
@@ -3,13 +3,13 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/mattchw/go-onboard/controllers"
-	middleware "github.com/mattchw/go-onboard/middlewares"
+	"github.com/mattchw/go-onboard/middlewares"
 )
 
 func UserRoute(app *fiber.App) {
 	app.Get("/users", controllers.GetUsers)
 	app.Get("/users/count", controllers.GetUsersCount)
-	app.Post("/users", middleware.AuthReq(), controllers.CreateUser)
+	app.Post("/users", middlewares.AuthReq(), controllers.CreateUser)
 	app.Get("/users/:userId", controllers.GetUser)
 	app.Patch("/users/:userId", controllers.UpdateUser)
 	app.Delete("/users/:userId", controllers.DeleteUser)
